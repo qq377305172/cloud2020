bcaff6a35df3b25f257d64612b0cb4f567ffc32f
[chaojing] <[chaojing1@aliyun.com]>
2020-04-02 18:25:47 +0800

    1

--- cloud-consumer-order80/src/main/java/jing/chao/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/jing/chao/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package jing.chao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
